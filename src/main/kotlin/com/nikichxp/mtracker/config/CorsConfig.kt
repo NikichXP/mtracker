c8e996e59b85a7036b6c14954257e7182b0176cd
nikichxp <3433520+NikichXP@users.noreply.github.com>
2026-09-03 23:53:26 +0200

    cors fix

--- src/main/kotlin/com/nikichxp/mtracker/config/CorsConfig.kt
+++ src/main/kotlin/com/nikichxp/mtracker/config/CorsConfig.kt
@@ -3,21 +3,22 @@ package com.nikichxp.mtracker.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 class CorsConfig {
 
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
+    fun corsWebFilter(): CorsWebFilter {
         val configuration = CorsConfiguration().apply {
             allowedOriginPatterns = listOf("*")
             allowedMethods = listOf("*")
             allowedHeaders = listOf("*")
         }
-        return UrlBasedCorsConfigurationSource().apply {
+        val source = UrlBasedCorsConfigurationSource().apply {
             registerCorsConfiguration("/**", configuration)
         }
+        return CorsWebFilter(source)
     }
 }
